09afe5fa58fa7f22df1c353d84c6486a54c5a84c
Yuriy Koziy <yukoziy@gmail.com>
2019-10-08 21:15:56 -0400

    Added /api/metadata path hadnler

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
 const port = 3000;
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
+});
+
+app.post('/api/metadata', upload.single('upfile'), (req, res) => {
+  const fileName = req.file.originalname;
+  const fileType = req.file.mimetype;
+  const fileSize = req.file.size;
+  res.json({name:fileName, type:fileType, size:fileSize});
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
